34766fb33b84bfc15862a9987615d05f92891285
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2018-10-13 18:17:12 +0300

    Testing a workflow and the API. Initial scripts

---
+++ echo_vars.sh
@@ -0,0 +1,19 @@
+export TESTVAR=mytestvar
+
+echo ""
+echo "We will echo the TESTVAR variable . It should return mytestvar"
+echo ""
+
+echo $TESTVAR
+
+echo ""
+echo "We will echo the TESTVAR2 variable . We have not defined it, so it will only show something if API works ok"
+echo ""
+
+echo $TESTVAR2
+
+echo ""
+echo "We will echo the TESTVAR3 variable . We have not defined it, so it will only show something if API works ok"
+echo ""
+
+echo $TESTVAR3
