9c2758935925d358811a5d6dbec82f91d15d897b
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2018-10-22 20:20:47 +0300

    Listing existing environmental values and modifying them

--- api_trigger_workflow.sh
+++ api_trigger_workflow.sh
@@ -7,6 +7,24 @@ value=${2:-master}
 user=${CIRCLE_PROJECT_USERNAME:-greenpeace}
 repo=${GITHUB_REPOSITORY_NAME:-planet4-test-circleci-api}
 
+echo "We will try to list the existing environment values"
+curl \
+  --header "Content-Type: application/json" \
+  -u "${CIRCLE_TOKEN}:" \
+  https://circleci.com/api/v1.1/project/${VCS_TYPE:-github}/${user}/${repo}/envvar
+
+json='{"name":"PROJECT_ENV_VALUE2", "value":"We managed to modify it!"}'
+
+echo ""
+echo "We will try to add an environment value named PROJECT_ENV_VALUE2"
+curl \
+  --header "Content-Type: application/json" \
+  -d "$json" \
+  -u "${CIRCLE_TOKEN}:" \
+  -X POST \
+  https://circleci.com/api/v1.1/project/${VCS_TYPE:-github}/${user}/${repo}/envvar
+
+
 echo "User:    ${user}"
 echo "Project: ${repo}"
 
