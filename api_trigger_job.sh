4899c258c1f37146bf550b5323fad136e60ff190
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2019-01-19 19:36:51 +0200

    switch to v2 and try to trigger a single job

---
+++ api_trigger_job.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+var=${1:-MyVar}
+value=${2:-MyValue}
+
+user=${CIRCLE_PROJECT_USERNAME:-greenpeace}
+repo=${GITHUB_REPOSITORY_NAME:-planet4-test-circleci-api}
+
+job=echo-vars-job
+
+echo ""
+echo "User:    ${user}"
+echo "Project: ${repo}"
+
+echo "Vef:     ${var}"
+echo "Value:   ${value}"
+
+json="{
+  \"$var\": \"$value\",
+  \"build_parameters\": {\"CIRCLE_JOB\": \"$job\" }
+}"
+echo ""
+echo "The json is"
+echo $json
+echo ""
+curl \
+  --header "Content-Type: application/json" \
+  -d "$json" \
+  -u "${CIRCLE_TOKEN}:" \
+  -X POST \
+  https://circleci.com/api/v1.1/project/${VCS_TYPE:-github}/${user}/${repo}/tree/master
